fecf14f65882315c41c5925a41be14daa34ef34f
Fabricio Silva <fabricionaweb@gmail.com>
2021-02-15 21:24:36 +0000

    start project

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import TimeFormat from "./web_modules/hh-mm-ss.mjs"
+
+const FORMAT = "hh:mm:ss"
+
+console.log(TimeFormat.toMs("0:00:25.48", FORMAT))
---
+++ web_modules/zeroFill.mjs
@@ -0,0 +1,14 @@
+// https://github.com/feross/zero-fill/blob/master/index.js
+function zeroFill(width, number, pad) {
+  if (number === undefined) {
+    return function (number, pad) {
+      return zeroFill(width, number, pad)
+    }
+  }
+  if (pad === undefined) pad = "0"
+  width -= number.toString().length
+  if (width > 0) return new Array(width + (/\./.test(number) ? 2 : 1)).join(pad) + number
+  return number + ""
+}
+
+export default zeroFill
