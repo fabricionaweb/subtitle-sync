f375538f1f5ba9a49b836a0357a82c9e8d5ade35
Fabricio Silva <fabricionaweb@gmail.com>
2021-02-20 00:35:25 +0000

    setup the tests

---
+++ src/main.mjs
@@ -0,0 +1,21 @@
+import { toMs, fromMs } from "../web_modules/hh-mm-ss.mjs"
+
+export const REGEX = /(\d+:\d+:\d+[.,]\d+)(,| --> )(\d+:\d+:\d+[.,]\d+)/gm
+const FORMAT = "h:mm:ss.ss"
+
+export const sum = (str, ms) => fromMs(toMs(str, FORMAT) + ms, FORMAT)
+export const shift = (content, ms) =>
+  content.replaceAll(REGEX, (_, marked, start, end) => {
+    return `Dialogue: ${marked},${sum(start, ms)},${sum(end, ms)}`
+  })
+
+export const download = (blob, fileName) => {
+  const href = URL.createObjectURL(blob)
+
+  const $anchor = document.querySelector("[data-download]")
+  $anchor.setAttribute("download", fileName)
+  $anchor.setAttribute("href", href)
+  $anchor.dispatchEvent(new MouseEvent("click"))
+
+  URL.revokeObjectURL(href)
+}
